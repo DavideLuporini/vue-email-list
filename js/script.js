5461d7012080357799fc7a06c4b325955658e08d
illupovero <davideluporini1996@gmail.com>
2022-02-06 20:52:36 +0100

    ended exercise

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
 console.log('hello world')
 
-var app = new Vue({
-    el: '#app',
+
+/* 
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+|Usando Vue.js, generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+|Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+|Evitare i doppioni nella lista delle mail 
+*/
+const app = new Vue({
+    el: "#app",
     data: {
-        message: 'Hello Vue!'
-    }
-})
+        emails: [],
+    },
+    methods: {
+        getEmail() {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res) => {
+                this.emails.push(res.data.response);
+
+            });
+        },
+        getRandomEmails() {
+            for (let i = 0; i < 10; i++) {
+                this.getEmail();
+            }
+        },
+    },
+    mounted() {
+        this.getRandomEmails();
+    },
+});
